8dbbf7db8c587da9f8331001de3edb0c5bd6ce47
Ksushkins <Ksushkins1992@gmail.com>
2014-09-08 18:57:10 +0600

    создание

---
+++ src/MathTest.java
@@ -0,0 +1,26 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: student
+ * Date: 08.09.14
+ * Time: 15:20
+ * To change this template use File | Settings | File Templates.
+ */
+public class MathTest {
+    @Test
+    public void testNotEquals()
+    {
+        Assert.assertFalse(5 == 25 / 3);
+    }
+
+    @Test
+    public void testEquals()
+    {
+        Assert.assertEquals(20, 5 * 5);
+
+
+        Assert.assertTrue(20 == 4 * 6);
+    }
+}
